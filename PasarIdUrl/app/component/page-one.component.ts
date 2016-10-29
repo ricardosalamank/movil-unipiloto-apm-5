1132a3d96992b325db3adcf2a59dd72e4b636d5b
ricardosalamank <rasalamancam@gmail.com>
2016-10-28 21:54:08 -0500

    pasar id formadinamica con servicios

---
+++ PasarIdUrl/app/component/page-one.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'page-one',
+    templateUrl: 'app/templates/page-one.html'
+})
+
+export class PageOneComponent {
+    title = 'Pagina 1';
+}
+
---
+++ PasarIdUrl/app/mock/user-mock.ts
@@ -0,0 +1,21 @@
+import {User} from '../model/user';
+export const PRODUCTS: User[] = [
+    {
+        id: 1,
+        name: "ricardo",
+        direccion: "cll52",
+        telefono: "123456"
+    },
+    {
+        id: 2,
+        name: "jose",
+        direccion: "cll falsa",
+        telefono: "987456"
+    },
+    {
+        id: 3,
+        name: "andrea",
+        direccion: "cll santaisabel",
+        telefono: "654541"
+    }
+];
